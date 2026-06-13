a0ba7f29c06c0c4cc2d0c343ed4bdfa8630bb9a2
Parasite <loneahmad110025@gmail.com>
2026-06-13 23:19:09 +0530

    Added helper functions for date and amount validation. Also did basic code formatting.

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+from datetime import datetime
 
 transactions = []
 next_id = 1
 
-def add_transcation(transaction_type):
+#helper functions
+def get_valid_date():
+    while True:
+        date = input("Enter the Date (DD/MM/YYYY)")
+        
+        try:
+            datetime.strptime(date, "%d/%m/%Y")
+            return date
+            
+        
+        except ValueError:
+            print("Invalid Date.")
+
+def get_valid_amount():
+      while True:      #To make sure a valid integer is entered.
+        amount = input("Enter the Amount:")
+        try:
+            amount = float(amount)
+            return amount 
+        except ValueError:
+            print("Please enter a valid Number")
+
+#Core functions
+
+def add_transaction(transaction_type):
     global next_id
 
-    date = input("Enter the Date (DD/MM/YYYY): ")
+    date = get_valid_date()
     category = input("Enter the Category: ")
     description = input("Enter the Description: ")
-    amount = float(input("Enter the Amount:"))
+    amount = get_valid_amount()
 
     transaction = {
         "id": next_id,
@@ -25,13 +50,16 @@ def add_transcation(transaction_type):
 
     print("Transaction added successfully!")
 
-    
 
-def del_transcation():
+def del_transaction():
     pass
 
 def view_transactions(): 
+    if not transactions:
+        print("No transactions found.")
+
     for transaction in  transactions:
+        print("-" * 30)
         print(f"Transaction ID: #{transaction['id']}")
         print(f"Type: {transaction['type']}")
         print(f"Category: {transaction['category']}")
@@ -47,20 +75,30 @@ def main():
         "\n2. View Transactions"
         "\n3. Exit"
         "\n>> "
-)
+    )
         
         if choice == "1":
-            txn_type = input("\nSelect transaction type (Expense/Revenue): ").strip().lower()
-            add_transcation(transaction_type=txn_type)
+            while True:        #Make sure only Expense or Income is entered.
+                txn_type = input("\nSelect transaction type (Expense/Income): ").strip().lower()
+                if txn_type in ["expense" , "income"]:
+                    add_transaction(transaction_type=txn_type)
+                    break
+                print("Please enter Expense or Income.")
 
                     
-        if choice == "2":
+        elif choice == "2":
             view_transactions()
 
-        if choice == "3":
-            pass
+        elif choice == "3":
+            break
 
-main()
+        else:
+            print("Invalid choice.")
+
+
+
+if __name__ == "__main__":
+    main()
 
 
         
